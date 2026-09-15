e0753163af7b2374e40bbfc67808dd491d66a79c
Mayank Chaturvedi <mayank.chatur.0.7@gmail.com>
2026-09-15 22:32:21 +0530

    Added comments

---
+++ LinkedList/Solution.java
@@ -0,0 +1,20 @@
+//Tortois and Hare Algorithm
+public class Solution 
+{
+    public boolean hasCycle(ListNode head) 
+    {
+        
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            return true;
+        }
+
+        return false;
+    }
+}
